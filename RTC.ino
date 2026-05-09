7f980d8500f2649b1a8b52ffbad130957041054e
1105-Monroe-Cydney <cydneym@unr.edu>
2026-05-08 18:37:26 -0700

    Create RTC.ino

---
+++ RTC.ino
@@ -0,0 +1,92 @@
+#include <RTClib.h>
+#include <Wire.h>
+#include <stdlib.h>
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday"};
+
+//UART functions-------------
+//intialize 9600
+void UART_init(unsigned int ubrr){
+
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+
+  UCSR0B = (1 << TXEN0); //EN transmitter
+  UCSR0C = (1<< UCSZ01)|(1<<UCSZ00); //8-bit data
+}
+
+//This should send one character 
+void UART_sendChar(char data){
+  while (!(UCSR0A & (1 << UDRE0)));
+
+  UDR0 = data;
+}
+
+//SHould send string 
+void UART_sendString(const char *str){
+  while (*str){
+    UART_sendChar(*str);
+    str++;
+  }
+}
+//Delay 
+//temp replacement
+
+//void simpleDelay(){
+ // for (volatile long i = 0; i<300000; i++);
+//}
+
+void setup() {
+  
+UART_init(103);
+  
+rtc.begin();
+  
+UART_sendString("DS1307RTC Read Test");
+UART_sendString("-------------------");
+}
+
+void loop() {
+DateTime now = rtc.now();
+  char buffer[12];
+  
+//for the YEAR
+itoa(now.year(), buffer, 10);
+UART_sendString(buffer);
+UART_sendChar('/');
+  
+//for the MONTH
+itoa(now.month(), buffer, 10);
+UART_sendString(buffer);
+UART_sendChar('/');
+  
+//for the DAY
+itoa(now.day(), buffer, 10);
+UART_sendString(buffer);
+UART_sendChar('/');
+  
+//day of the week
+UART_sendString("(");
+UART_sendString(daysOfTheWeek[now.dayOfTheWeek()]);
+UART_sendString(")");
+  
+//for the HOUR
+UART_sendString(" ");
+itoa(now.hour(), buffer, 10);
+UART_sendString(buffer);
+UART_sendChar(':');
+
+//for the Minute
+itoa(now.minute(), buffer, 10);
+UART_sendString(buffer);
+UART_sendChar(':');
+
+//for the Second
+itoa(now.second(), buffer, 10);
+UART_sendString(buffer);
+  
+UART_sendString("\r\n");
+delay(60000);
+//simpleDelay();
+}
